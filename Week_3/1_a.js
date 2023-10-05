69218eb892c2e334cc37ddc52789df90300ddc5d
NgocLinh3110 <linhvo31102002@gmail.com>
2023-10-05 20:37:13 +0700

    load app first screen

---
+++ Week_3/1_a.js
@@ -0,0 +1,93 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { useFonts } from "expo-font";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+  });
+  return (
+    <View style={styles.container}>
+      <View style={styles.gradient} />
+      <StatusBar style="auto" />
+      <View style={styles.style1}>
+        <Image
+          style={styles.logo}
+          source={require("./pic/Ellipse 8.png")}
+        ></Image>
+      </View>
+      <View style={styles.style2}>
+        <Text style={styles.slogan}>GROW YOUR BUSSINESS</Text>
+      </View>
+      <View style={styles.style3}>
+        <Text style={styles.text1}>
+          We will help you to grow your business using online server
+        </Text>
+        <View style={styles.button}>
+          <Button title="Login" color="#E3C000" style={styles.b} />
+          <Button title="Sign up" color="#E3C000" style={styles.b} />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gradient: {
+    flex: 1,
+    width: "100%",
+    backgroundImage: "linear-gradient(to bottom, #C7EFF7, #04CDF9)",
+  },
+  style1: {
+    flex: 1,
+  },
+  style2: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  style3: {
+    flex: 1,
+  },
+  button: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  b: {
+    width: "119px",
+    height: "48px",
+    alignItems: "center",
+  },
+  logo: {
+    width: "140px",
+    height: "140px",
+    justifyContent: "center",
+    marginTop: "105px",
+  },
+  slogan: {
+    fontFamily: "Roboto-Black",
+    fontSize: "25px",
+    lineHeight: "29.3px",
+    textAlign: "center",
+    width: "100%",
+    height: "58px",
+    fontWeight: "bold",
+    marginTop: "87px",
+    alignContent: "center",
+  },
+  text1: {
+    width: "302px",
+    height: "36px",
+    fontFamily: "Roboto-Black",
+    fontWeight: "700",
+    fontSize: "15",
+    lineHeight: "17.58px",
+    textAlign: "center",
+    alignContent: "flex-end",
+  },
+});
